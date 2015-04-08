09796b5ccc05e890c32e3028450bd7031d246c1b
Tiago Martinho <tiago.martinho@xpeppers.com>
2015-04-08 14:29:07 +0200

    add roll and score methods, implemented first test

--- BowlingGame/test/BowlingGameTest.java
+++ BowlingGame/test/BowlingGameTest.java
@@ -1,13 +1,15 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
-
 
 public class BowlingGameTest {
 
 	@Test
 	public void testGutterGame() throws Exception {
 		Game g = new Game();
+		for(int i=0;i<20;i++){
+			g.roll(0);
+		}
+	    assertEquals(0, g.score());
 	}
 
 }
