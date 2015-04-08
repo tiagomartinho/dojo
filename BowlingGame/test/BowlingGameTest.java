d14418528b1808eb51b16ae83d0248fdcab84e06
Tiago Martinho <tiago.martinho@xpeppers.com>
2015-04-08 14:30:16 +0200

    add second test

--- BowlingGame/test/BowlingGameTest.java
+++ BowlingGame/test/BowlingGameTest.java
@@ -12,4 +12,12 @@ public class BowlingGameTest {
 	    assertEquals(0, g.score());
 	}
 
+	@Test
+	public void testAllOnes() throws Exception {
+		Game g = new Game();
+		for(int i=0;i<20;i++){
+			g.roll(1);
+		}
+	    assertEquals(20, g.score());
+	}
 }
