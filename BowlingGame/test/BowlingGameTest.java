7f66c3df02d3b21bc15a3ade02ca2b458ed23630
tiagomartinho <tiago.martinho@xpeppers.com>
2015-04-14 14:24:31 +0200

    extract roll spare method in bowling kata

--- BowlingGame/test/BowlingGameTest.java
+++ BowlingGame/test/BowlingGameTest.java
@@ -32,10 +32,14 @@ public class BowlingGameTest {
 	
 	@Test
 	public void testOneSpare() throws Exception {
-		g.roll(5);
-		g.roll(5); //spare
+		rollSpare();
 		g.roll(3);
 		rollMany(17,0);
 	    assertEquals(16, g.score());
+	}
+
+	private void rollSpare() {
+		g.roll(5);
+		g.roll(5);
 	}
 }
