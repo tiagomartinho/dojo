ba928de92e2c9f37b9b7df45edc03a8c7db503f6
tiagomartinho <tiago.martinho@xpeppers.com>
2015-04-14 14:21:12 +0200

    pass third test of bowling kata

--- BowlingGame/src/Game.java
+++ BowlingGame/src/Game.java
@@ -1,17 +1,21 @@
 public class Game {
 	private int rolls[] = new int[21];
 	private int currentRoll = 0;
-	
+
 	public void roll(int pins) {
-		rolls[currentRoll++]=pins;
+		rolls[currentRoll++] = pins;
 	}
 
 	public int score() {
 		int score = 0;
 		int i = 0;
-		for(int frame=0;frame<10;frame++){
-			score += rolls[i] + rolls[i+1];
-			i+=2;
+		for (int frame = 0; frame < 10; frame++) {
+			if (rolls[i] + rolls[i + 1] == 10) { //spare
+				score += 10 + rolls[i + 2];
+			} else {
+				score += rolls[i] + rolls[i + 1];
+			}
+			i += 2;
 		}
 		return score;
 	}
