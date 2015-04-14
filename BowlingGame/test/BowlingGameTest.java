ea5aea35d87e8ce93c940cd859263faddc1b0cbb
tiagomartinho <tiago.martinho@xpeppers.com>
2015-04-14 14:11:52 +0200

    add third test to bowling kata

--- BowlingGame/test/BowlingGameTest.java
+++ BowlingGame/test/BowlingGameTest.java
@@ -29,4 +29,13 @@ public class BowlingGameTest {
 		rollMany(20,1);
 	    assertEquals(20, g.score());
 	}
+	
+	@Test
+	public void testOneSpare() throws Exception {
+		g.roll(5);
+		g.roll(5); //spare
+		g.roll(3);
+		rollMany(17,0);
+	    assertEquals(16, g.score());
+	}
 }
