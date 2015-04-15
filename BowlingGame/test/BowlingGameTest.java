f901ed587738d64728e1b027fd397222031483aa
tiagomartinho <tiago.martinho@xpeppers.com>
2015-04-15 14:14:36 +0200

    add fifth test in bowling kata tests

--- BowlingGame/test/BowlingGameTest.java
+++ BowlingGame/test/BowlingGameTest.java
@@ -55,4 +55,10 @@ public class BowlingGameTest {
 	private void rollStrike() {
 		g.roll(10);
 	}
+	
+	@Test
+	public void testPerfectGame() throws Exception {
+		rollMany(12,10);
+	    assertEquals(300, g.score());
+	}
 }
