915c157a20f84d93d34fd0efbd8dba4b4591c765
tiagomartinho <tiago.martinho@xpeppers.com>
2015-04-20 14:16:41 +0200

    add swift bowling kata with first test

---
+++ BowlingKata/BowlingKata/BowlingGame.swift
@@ -0,0 +1,20 @@
+//
+//  BowlingGame.swift
+//  BowlingKata
+//
+//  Created by Martinho on 20/04/15.
+//  Copyright (c) 2015 Martinho. All rights reserved.
+//
+
+import Foundation
+
+class BowlingGame
+{
+    func roll(pins:Int){
+        
+    }
+    
+    func score()->Int{
+        return 0
+    }
+}
