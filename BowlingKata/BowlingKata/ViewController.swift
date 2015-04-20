172dea60e78d88f1cf9cab41b7ad7332361d6516
tiagomartinho <tiago.martinho@xpeppers.com>
2015-04-20 14:12:25 +0200

    add git ignore for swift

---
+++ BowlingKata/BowlingKata/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BowlingKata
+//
+//  Created by Martinho on 20/04/15.
+//  Copyright (c) 2015 Martinho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
