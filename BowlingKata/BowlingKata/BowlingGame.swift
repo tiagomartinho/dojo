8185a345664f5d7d006dc0e3e520051f207d8b07
tiagomartinho <tiago.martinho@xpeppers.com>
2015-04-24 14:23:00 +0200

    extract is strike method

--- BowlingKata/BowlingKata/BowlingGame.swift
+++ BowlingKata/BowlingKata/BowlingGame.swift
@@ -20,16 +20,17 @@ class BowlingGame
         var score = 0
         var frameIndex = 0
         for _ in 1...frames {
-            if rolls[frameIndex] == 10 {
+            if isStrike(frameIndex) {
                 score += 10 + rolls[frameIndex+1] + rolls[frameIndex+2]
             }
-            else { if isSpare(frameIndex) {
-                score += 10 + rolls[frameIndex+2]
-                frameIndex++
-            }
-            else{
-                score += rolls[frameIndex] + rolls[frameIndex+1]
-                frameIndex++
+            else {
+                if isSpare(frameIndex) {
+                    score += 10 + rolls[frameIndex+2]
+                    frameIndex++
+                }
+                else{
+                    score += rolls[frameIndex] + rolls[frameIndex+1]
+                    frameIndex++
                 }
             }
             frameIndex++
@@ -39,6 +40,10 @@ class BowlingGame
     
     func roll(pins:Int){
         rolls[currentRoll++]=pins
+    }
+    
+    func isStrike(frameIndex:Int)->Bool{
+        return rolls[frameIndex] == 10
     }
     
     func isSpare(frameIndex:Int)->Bool{
