d47dae19ceb1fa112d96d4d53d7c82deec31c3f5
Tiago Martinho <tiago.martinho@xpeppers.com>
2015-05-06 14:21:25 +0200

    add second test to tennis kata

--- test_tennis_scoring.py
+++ test_tennis_scoring.py
@@ -1,9 +1,17 @@
 import pytest
 
 class TennisGame:
+	PLAYER_ONE = 0
+	def add_point(self,player):
+		pass
 	def score(self):
 		return (0,0) 
 #TESTS
 def test_empty_game():
 	game = TennisGame()
 	assert (game.score()[0] == 0 and game.score()[1] == 0) 
+
+def test_first_player_makes_first_point():
+	game = TennisGame()
+	game.add_point(TennisGame.PLAYER_ONE)
+	assert (game.score()[0] == 15 and game.score()[1] == 0) 
