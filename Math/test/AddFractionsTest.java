e802ed45767149df13a3f7087f27d15ae281c58e
Tiago Martinho <tiago.martinho@xpeppers.com>
2015-05-19 14:16:19 +0200

    added third test to fractions kata in java

--- Math/test/AddFractionsTest.java
+++ Math/test/AddFractionsTest.java
@@ -16,4 +16,10 @@ public class AddFractionsTest {
 		Fraction sum = new Fraction(3).plus(new Fraction(0));
 		assertEquals(3, sum.intValue());
 	}
+	
+	@Test
+	public void zeroPlusNonZero() throws Exception {
+		Fraction sum = new Fraction(0).plus(new Fraction(5));
+		assertEquals(5, sum.intValue());
+	}
 }
