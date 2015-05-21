8ed262d9863aee24516d5106cf2a291ec2d5b8cc
Tiago Martinho <tiago.martinho@xpeppers.com>
2015-05-21 14:12:13 +0200

    make gcd algorithm return always positive number

--- Math/test/GreatestCommonDivisorTest.java
+++ Math/test/GreatestCommonDivisorTest.java
@@ -8,14 +8,14 @@ public class GreatestCommonDivisorTest {
 	public void reflexive() throws Exception {
 		assertEquals(1, gcd(1, 1));
 		assertEquals(2, gcd(2, 2));
-		assertEquals(-1, gcd(-1, -1));
+		assertEquals(1, gcd(-1, -1));
 	}
 
 	@Test
 	public void relativelyPrimes() throws Exception {
 		assertEquals(1, gcd(2,3));
 		assertEquals(1, gcd(4,7));
-		assertEquals(-1, gcd(-2,-3));
+		assertEquals(1, gcd(-2,-3));
 	}
 	
 	@Test
@@ -24,13 +24,25 @@ public class GreatestCommonDivisorTest {
 		assertEquals(5, gcd(5,30));
 	}
 	
+	@Test
+	public void commonFactor() throws Exception {
+		assertEquals(2, gcd(6,8));
+		assertEquals(7, gcd(49,315));
+	}
+	
+	@Test
+	public void negatives() throws Exception {
+		assertEquals(4, gcd(-24,28));
+		assertEquals(4, gcd(24,-28));
+	}
+	
 	private int gcd(int a, int b) {
 		while(b!=0){
 			int t = b;
 			b = a % t;
 			a = t;
 		}
-		return a;
+		return Math.abs(a);
 	}
 
 }
